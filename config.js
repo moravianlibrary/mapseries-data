e3fc10b8b354de6230f686c54ae10db548fbbf58
mila.pacek <mila.pacek@gmail.com>
2022-10-24 11:27:29 +0200

    Updated Evropa; Třetí vojenské mapování, topografické sekce 1:25 000; Befestigungskarte Tschechoslowakei 1:25 000 serie

--- config.js
+++ config.js
@@ -96,8 +96,8 @@ mapseries.config = {
     },
     {
       title: "Evropa; Třetí vojenské mapování, topografické sekce 1:25 000; Karte des Sudetenlandes 1:25 000",
-      layer: "evropa-befestigungskarte-tschechoslowakei-125-000",
-      template: "evropa-befestigungskarte-tschechoslowakei-125-000.txt",
+      layer: "evropa-karte-des-sudetenlandes-125-000",
+      template: "evropa-karte-des-sudetenlandes-125-000.txt",
       formatFunctions: {
         addDegrees: function (stringValue, sheet) {
           stringValue += '';
