0c2d894755eecf7e53c73de2478cead017e6b734
DominikaVodakova <dominika.vodakova@gmail.com>
2026-02-23 15:01:10 +0100

    Updated Evropa; Základní mapa 1:10 000; Základní mapa ČSSR 1:10 000 serie

--- config.js
+++ config.js
@@ -527,6 +527,20 @@ mapseries.config = {
           return stringValue;
         }
       }
+    },
+    {
+      title: "Evropa; Základní mapa 1:10 000; Základní mapa ČSSR 1:10 000",
+      layer: "evropa-zakladni-mapa-cssr-110-000",
+      template: "evropa-zakladni-mapa-cssr-110-000.txt",
+      formatFunctions: {
+        addDegrees: function (stringValue, sheet) {
+          stringValue += '';
+          if(stringValue.length==4) {
+            stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
+          }
+          return stringValue;
+        }
+      }
     }
   ]
 }
